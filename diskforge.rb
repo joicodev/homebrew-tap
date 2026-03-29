fa85c9c5358646df303ec81b50d99b0300624adb
Roimer Peraza <roimerj63@gmail.com>
2026-03-29 11:47:54 -0400

    feat: use real SHA256 for apple silicon v0.6.0

--- diskforge.rb
+++ diskforge.rb
@@ -7,15 +7,14 @@ class Diskforge < Formula
   on_macos do
     on_arm do
       url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-aarch64-apple-darwin.tar.gz"
-      # To be updated with: shasum -a 256 diskforge-v0.6.0-aarch64-apple-darwin.tar.gz
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "5ef9d548bdaf15686e87cb44ca62efd8c1c668923d49060a077eaccb7ed74ccf"
     end
 
-    on_intel do
-      url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-x86_64-apple-darwin.tar.gz"
-      # To be updated with: shasum -a 256 diskforge-v0.6.0-x86_64-apple-darwin.tar.gz
-      sha256 "REPLACE_WITH_X86_64_SHA256"
-    end
+    # Intel support disabled until binaries are generated
+    # on_intel do
+    #   url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-x86_64-apple-darwin.tar.gz"
+    #   sha256 "REPLACE_WITH_X86_64_SHA256"
+    # end
   end
 
   def install
