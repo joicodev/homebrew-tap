7ee6e604cc64fb7708bebfcdbb5dfebcc8022975
Roimer Peraza <roimerj63@gmail.com>
2026-03-29 20:11:16 -0400

    chore: update diskforge formula to 0.8.3

--- diskforge.rb
+++ diskforge.rb
@@ -1,20 +1,19 @@
 class Diskforge < Formula
   desc "Intelligent disk cleanup CLI for developers"
   homepage "https://github.com/joicodev/diskforge"
-  version "0.6.0"
+  version "0.8.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "5ef9d548bdaf15686e87cb44ca62efd8c1c668923d49060a077eaccb7ed74ccf"
+      sha256 "d340b38519ecaec72c76121d6291fca0b1e68ee34c50b9781f1322758ec4b44a"
     end
 
-    # Intel support disabled until binaries are generated
-    # on_intel do
-    #   url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-x86_64-apple-darwin.tar.gz"
-    #   sha256 "REPLACE_WITH_X86_64_SHA256"
-    # end
+    on_intel do
+      url "https://github.com/joicodev/diskforge/releases/download/v#{version}/diskforge-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "cb0a6ea25d4d76bbc8942e6d719a1298201dbfbcb9d42f395964a4e004369016"
+    end
   end
 
   def install
